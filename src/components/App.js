c7ef6bec25eb56d0eb08972f5ed1a6b53aa42427
ikennaumeh <kennaumeh@gmail.com>
2021-12-31 12:29:05 +0100

    updates

--- src/components/App.js
+++ src/components/App.js
@@ -52,11 +52,11 @@ class App extends Component {
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
+            href=""
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dapp University
+            ikennaumeh
           </a>
         </nav>
         <div className="container-fluid mt-5">
